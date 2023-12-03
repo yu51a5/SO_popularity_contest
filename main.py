a56851224910d02a379eb1208abc563da582164f
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-12-03 00:49:13 +0000

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from plot_pies import plot_pies
 
-plot_pies(filename="Containers.csv",
-          annotation_angle_adjustments={'question_count' : {'tuples' : 0.9}, 'view_count' : {'tuples' : 0.9, 'list' : 0.2}})
+################################################################################################
+# filename is the name of the file that contains data
+# it has 4 columns: key_word, viewed, question, unanswered_question
+# it can be generated with https://data.stackexchange.com/stackoverflow/query/1805161
+
+# start_angles - the angle (degrees, between 0 and 360) at which to start the pie chart. 
+
+# annotation_angle_adjustments is used to adjest the angle of annotations arrows.
+# It's optional, only set it if you are unhappy with the angles.
+# The values should be between 0.0 and 1.0.
+# By default, it's set to 0.5. Values smaller than 0.5 will move it clockwise,
+# and greater than 0.5 will move it counter-clockwise
+
+################################################################################################
+containers_params = dict(filename="Containers.csv", title="Python Containers",
+                         start_angles={'question' : 15, 'viewed' : 10},
+                         annotation_angle_adjustments={'question' : {'tuples' : 0.9, 'list' : 0.4}, 
+                                                       'viewed' : {'tuples' : 0.9, 'list' : 0.2}})
+matplotlib_params = dict(filename="Matplotlib.csv", title="Python Plotting",
+                         start_angles={'question' : 240, 'viewed' : 30},
+                         annotation_angle_adjustments={'viewed' : {'bokeh' : 1., 'plotly' : 0.65}})
+
+plot_pies(**matplotlib_params)
 
 print('all done!')
--- plot_pies.py
+++ plot_pies.py
@@ -8,12 +8,13 @@
 #url = f"https://api.stackexchange.com/{addon}&site=stackoverflow&access_token={os.environ['access_token']}&key={os.environ['key']}"
 #print(requests.get(url).text)
 
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.patches import Wedge
-import numpy as np
 import os
 from datetime import datetime
+
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 my_color_palette = { 'superGold'   : '#ffb000',
                      'lightBlue'   : "#d1d1ff",
@@ -31,40 +32,53 @@ kw = dict(arrowprops=dict(arrowstyle="-"),
           zorder=0, 
           va="center")
 
-def plot_pies(filename, annotation_angle_adjustments={}, figsize=(8, 5), startangle=15, dpi=300, image_extension='png'):
+def plot_pies(filename, title=None, annotation_angle_adjustments={}, fig_height=5, fig_width={'question':10, 'viewed': 10}, start_angles={}, dpi=300, image_extension='png'):
   
   last_modif_date = datetime.fromtimestamp(os.path.getmtime(filename)).strftime('%d %b %Y')
+  if title is None:
+    title = filename[:filename.find('.')].replace('_', ' ').title()
 
+  # reorder data
   df = pd.read_csv(filename)
   df['new_order'] = - abs(-0.25 + 0.5 * len(df) - df.index.to_numpy())
   df = df.set_index('key_word')
   df = df.sort_values('new_order')
-  df['unanswered_rate'] = df['unanswered_question'] / df['question_count']
-  df = df.drop(['new_order', 'unanswered_rate'], axis=1)
-  
+  df['unanswered_rate'] = df['unanswered_question'] / df['question']
+  df = df.drop(['new_order', 'unanswered_question'], axis=1)
+
+  # check annotation_angle_adjustments key validity
   errors = {'columns' : [e for e in annotation_angle_adjustments if e not in df.columns], 
             'rows' : [k for v in annotation_angle_adjustments.values() for k in v.keys() if k not in df.index.values]}
   assert not errors['columns'] and not errors['rows'], f"Errors in ang_mult_dicts: {errors}"
   assert len(df.index.values) <= len(my_color_palette), \
          f"{len(df.index.values)} colors are required but only defined {len(my_color_palette)} colors are defined"
-  
+
+  # plot
   for col_name in df.columns:
-    if col_name.startswith('u'):
+    if col_name == 'unanswered_rate':
       continue
     ang_mult_dict = annotation_angle_adjustments.get(col_name, {})
   
-    plot = df.plot.pie(y=col_name, figsize=figsize, startangle=startangle,
-                      colors=my_color_palette.values(),
-                      labels=[''] * len(df.index))
-    wedges = [w for w in plot.get_children() if type(w) is Wedge]
-    if 'view' not in col_name.lower():
-      pass
-      
-    #plot.legend(labels=df.index, loc="upper left", bbox_to_anchor=(-0.4, 0.5))
-    plot.set_title(f"Stackoverflow Python Questions {col_name.replace('_', ' ').title()}\nas of {last_modif_date}")
+    plot = df.plot.pie(y=col_name, figsize=(fig_width[col_name], fig_height), startangle=start_angles.get(col_name, 0),
+                       colors=my_color_palette.values(),
+                       labels=[''] * len(df.index))
+    wedges = [w for w in plot.get_children() if type(w) is mpatches.Wedge]
+
+    # add a black pie chart
+    if col_name.lower() == 'question':
+      df.plot.pie(y=col_name, startangle=start_angles.get(col_name, 0),
+                  colors=['black']*len(df.index), wedgeprops={'width':0.3}, 
+                  labels=['']*len(df.index), ax=plot)
+      black_wedges = [w for w in plot.get_children() if (type(w) is mpatches.Wedge) and w not in wedges]
+      for how_many_unanswered, w in zip(df['unanswered_rate'], black_wedges):
+        w.set_width(w.r*how_many_unanswered)
+    
+    # title and legend
+    plot.set_title(f"Stackoverflow {title} {col_name.upper()} Count\nas of {last_modif_date}")
     plot.yaxis.label.set_visible(False)
     plot.get_legend().remove()
-  
+
+    # add annotations
     for label_, how_many, p in zip(df.index, df[col_name], wedges):
       mult_ang = ang_mult_dict[label_] if label_ in ang_mult_dict else 0.5
       ang = (p.theta2 - p.theta1)*mult_ang + p.theta1
@@ -72,7 +86,13 @@ def plot_pies(filename, annotation_angle_adjustments={}, figsize=(8, 5), startan
       y = np.sin(ang2)
       x = np.cos(ang2)
       kw["arrowprops"]["connectionstyle"] =  f"angle,angleA=0,angleB={ang}"
-      plot.annotate(f'{label_}\n{how_many:,} {col_name[:col_name.find("_")]}s', xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y), horizontalalignment="left" if x >= 0 else "right", **kw)
-  
+      ann = f'{label_}\n'
+      if col_name.lower() == 'question':
+        ann += f'{how_many:,} questions, {int(100*df.loc[label_, "unanswered_rate"])}% unanswered'
+      else:
+        ann += f'viewed {how_many:,} times'
+      plot.annotate(ann, xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y), horizontalalignment="left" if x >= 0 else "right", **kw)
+
+    # save as a file
     plt.savefig(f'{filename[:filename.find(".")]}_{col_name}.{image_extension}', dpi=300)
     
